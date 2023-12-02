6e52f7351c2ac729e758b4e78ba073ac5de06b45
Neal van Veen <Nvveen@users.noreply.github.com>
2023-12-02 17:02:45 +0000

    enable solution day2

--- day2/src/lib.rs
+++ day2/src/lib.rs
@@ -1,38 +1,34 @@
-pub fn day2_1(input: &str) -> u32 {
-    let valid_games = input
+fn sum_valid_games(input: &str) -> u32 {
+    input
         .lines()
         .filter_map(|line| {
-            let mut parts = line.split(":");
+            let mut parts = line.split(" ");
             let id = parts
-                .next()
-                .unwrap()
-                .trim()
-                .split(" ")
                 .nth(1)
                 .unwrap()
+                .trim_end_matches(":")
                 .parse::<u32>()
                 .unwrap();
-            let turns = parts.next().unwrap().trim().split(";");
-            for turn in turns {
-                let grabs = turn.trim().split(",");
-                for grab in grabs {
-                    let mut grab_parts = grab.trim().split(" ");
-                    let count = grab_parts.next().unwrap().parse::<u32>().unwrap();
-                    let color = grab_parts.next().unwrap().trim();
-                    if color == "red" && count > 12 {
-                        return None;
-                    } else if color == "green" && count > 13 {
-                        return None;
-                    } else if color == "blue" && count > 14 {
-                        return None;
-                    }
+            // Every odd word is a count, every even word is a color
+            while let Some(word) = parts.next() {
+                let count = word.parse::<u32>().unwrap();
+                let color = parts.next().unwrap().trim_end_matches([',', ';']);
+                if color == "red" && count > 12 {
+                    return None;
+                } else if color == "green" && count > 13 {
+                    return None;
+                } else if color == "blue" && count > 14 {
+                    return None;
                 }
             }
-            println!("Game {} is valid", id);
-            return Some(id);
+            Some(id)
         })
-        .sum();
-    valid_games
+        .sum()
+}
+
+pub fn day2_1() -> u32 {
+    let input = include_str!("input");
+    sum_valid_games(input)
 }
 
 #[cfg(test)]
@@ -46,6 +42,6 @@ Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
 Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
 Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
 Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"#;
-        assert_eq!(day2_1(input), 8);
+        assert_eq!(sum_valid_games(input), 8);
     }
 }
---
+++ day2/src/main.rs
@@ -0,0 +1,5 @@
+use day2::day2_1;
+
+fn main() {
+    println!("Day 2 solution 1: {}", day2_1());
+}
