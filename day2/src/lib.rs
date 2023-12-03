bdf0b3c4a58fac2ba500f44b59ab1544b45ee3d2
Neal van Veen <Nvveen@users.noreply.github.com>
2023-12-03 15:26:13 +0000

    add day 2 solution 2

--- day2/src/lib.rs
+++ day2/src/lib.rs
@@ -26,9 +26,29 @@ fn sum_valid_games(input: &str) -> u32 {
         .sum()
 }
 
+fn power_valid_games(input: &str) -> u32 {
+    input.lines().fold(0, |acc, line| {
+        let mut parts = line.split(" ").skip(2);
+        let (mut red, mut green, mut blue) = (0, 0, 0);
+        while let Some(word) = parts.next() {
+            let count = word.parse::<u32>().unwrap();
+            let color = parts.next().unwrap().trim_end_matches([',', ';']);
+            red = if color == "red" && count > red { count } else { red };
+            green = if color == "green" && count > green { count } else { green };
+            blue = if color == "blue" && count > blue { count } else { blue };
+        }
+        acc + (red * green * blue)
+    })
+}
+
+const INPUT: &str = include_str!("input");
+
 pub fn day2_1() -> u32 {
-    let input = include_str!("input");
-    sum_valid_games(input)
+    sum_valid_games(INPUT)
+}
+
+pub fn day2_2() -> u32 {
+    power_valid_games(INPUT)
 }
 
 #[cfg(test)]
@@ -36,12 +56,22 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
+    fn test_sum_valid_games() {
         let input = r#"Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
 Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
 Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
 Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
 Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"#;
         assert_eq!(sum_valid_games(input), 8);
+    }
+
+    #[test]
+    fn test_power_valid_games() {
+        let input = r#"Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"#;
+        assert_eq!(power_valid_games(input), 2286);
     }
 }
--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,5 +1,6 @@
-use day2::day2_1;
+use day2::{day2_1, day2_2};
 
 fn main() {
     println!("Day 2 solution 1: {}", day2_1());
+    println!("Day 2 solution 2: {}", day2_2());
 }
