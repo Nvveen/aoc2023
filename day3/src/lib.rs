5532fcc77f23c058e0dd5418a16aef82a5540a50
Neal van Veen <Nvveen@users.noreply.github.com>
2023-12-03 21:21:58 +0000

    add day 3 solution 2

--- day3/src/lib.rs
+++ day3/src/lib.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 use fancy_regex::Regex;
 
 fn sum_valid_numbers(input: &str) -> u32 {
@@ -46,19 +48,67 @@ fn sum_valid_numbers(input: &str) -> u32 {
     sum
 }
 
-const INPUT : &str = include_str!("input");
+fn gear_ratio(input: &str) -> u32 {
+    let r = Regex::new(r"(\d+|\*)").unwrap();
+    let captures = input
+        .lines()
+        .enumerate()
+        .flat_map(|(line_no, line)| r.find_iter(line).map(move |m| (line_no, m.unwrap())))
+        .collect::<Vec<_>>();
+    let mut sum = 0 as u32;
+
+    for (line_no, cap) in captures.iter().filter(|(_, cap)| cap.as_str() == "*") {
+        let mut acc = HashSet::new();
+        for i in -1..=1 {
+            for j in -1..=1 {
+                if i == 0 && j == 0 {
+                    continue;
+                }
+                let (x, y) = (cap.start() as i32 + i, *line_no as i32 + j);
+                let ch = input
+                    .lines()
+                    .nth(y as usize)
+                    .unwrap()
+                    .chars()
+                    .nth(x as usize)
+                    .unwrap();
+                if ch.is_numeric() {
+                    let num = captures
+                        .iter()
+                        .find(|c| {
+                            c.0 == y as usize && x >= c.1.start() as i32 && x < c.1.end() as i32
+                        })
+                        .unwrap()
+                        .1
+                        .as_str()
+                        .parse::<u32>()
+                        .unwrap();
+                    acc.insert(num);
+                }
+            }
+        }
+        if acc.len() == 2 {
+            sum += acc.iter().product::<u32>();
+        }
+    }
+    sum
+}
+
+const INPUT: &str = include_str!("input");
 
 pub fn day3_1() -> u32 {
     sum_valid_numbers(INPUT)
+}
+
+pub fn day3_2() -> u32 {
+    gear_ratio(INPUT)
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn test_sum_valid_numbers() {
-        let input = r#"467..114..
+    const TEST_INPUT: &str = r#"467..114..
 ...*......
 ..35..633.
 ......#...
@@ -68,6 +118,14 @@ mod tests {
 ......755.
 ...$.*....
 .664.598.."#;
-        assert_eq!(sum_valid_numbers(input), 4361);
+
+    #[test]
+    fn test_sum_valid_numbers() {
+        assert_eq!(sum_valid_numbers(TEST_INPUT), 4361);
+    }
+
+    #[test]
+    fn test_gear_ratio() {
+        assert_eq!(gear_ratio(TEST_INPUT), 467835);
     }
 }
