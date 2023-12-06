9812fcb0d38893c5d073f4a13e8dd3ee40953825
Neal van Veen <Nvveen@users.noreply.github.com>
2023-12-06 16:25:23 +0000

    add day6

---
+++ day6/src/lib.rs
@@ -0,0 +1,24 @@
+pub fn day6_1(input: &str) -> u64 {
+    0
+}
+
+pub fn day6_2(input: &str) -> u64 {
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const INPUT: &str = r#""#;
+
+    #[test]
+    fn test_day5_1() {
+        assert_eq!(day6_1(INPUT), 1);
+    }
+
+    #[test]
+    fn test_day5_2() {
+        assert_eq!(day6_1(INPUT), 1);
+    }
+}
---
+++ day6/src/main.rs
@@ -0,0 +1,8 @@
+use day6::{ day6_1, day6_2 };
+
+const INPUT: &str = include_str!("input");
+
+pub fn main() {
+    println!("Day 6 solution 1: {}", day6_1(INPUT));
+    println!("Day 6 solution 2: {}", day6_2(INPUT));
+}
